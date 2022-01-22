ed4d0ec05c50d42e22cc7287293ed89f9757e401
Manvinder Tejpal <90982683+Manvindertejpal5489@users.noreply.github.com>
2022-01-22 16:39:27 +0530

    chapter 1 completed

--- Chap02/class.py
+++ Chap02/class.py
@@ -2,15 +2,21 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 class Duck:
-    def quack(self):
-        print('Quaaack!')
+    sound = 'Quack!!'
+    walking = 'Walks like a duck.'
+# this class contains two functions
+# sometimes functions are called methods
+# sometimes variables are called properties
+    def quack(self): # The  first argument while defining a method inside a class is always NAMED self CONVENTIONALLY
+        print(self.sound)
 
     def walk(self):
-        print('Walks like a duck.')
-
+        print(self.walking)
+        
+# donald is a variable who we are assigning to the class Duck
 def main():
-    donald = Duck()
-    donald.quack()
-    donald.walk()
+    donald = Duck() # donald is now an object of the class duck
+    donald.quack() # . is the reference operator to reference members of the object donald to the method called quack()
+    donald.walk() # . is the reference operator to reference members of the object donald to the method called walk()
 
 if __name__ == '__main__': main()
